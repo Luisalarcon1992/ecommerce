306db799361293415f2f6ba5367151cf6f9b2914
Lui Alarcon <luisalarcon1992@gmail.com>
2023-12-26 11:27:17 -0300

    front casi terminado

--- components/cart/AppCart.vue
+++ components/cart/AppCart.vue
@@ -5,10 +5,10 @@
 </script>
 
 <template>
-    <section>
+    <section class="flex flex-row gap-4">
         <table class="relative">
-            <thead class="w-[872px] h-14 border border-solid border-primary bg-[#E2F4FF]">
-                <tr>
+            <thead class=" h-14 border border-solid border-primary bg-[#E2F4FF]">
+                <tr class="border-b border-solid border-primary">
                     <th class="texto pr-32">Producto</th>
                     <th class="texto pr-32">Precio</th>
                     <th class="texto pr-32">Cantidad</th>
@@ -17,12 +17,40 @@
                 </tr>
             </thead>
             <tbody>
-                <tr class="border-b boder-solid border-[#C3C3C3] my-5">
+                <tr class="relative">
                     <td class=" flex flex-row justify-center items-start gap-4 mt-5 mr-10">
-                        <figure class="p-3 border-[0.5px] border-solid border-[#BDBDBD] rounded-[20px]">
+                        <figure class="p-3 border-[0.5px] mb-3 border-solid border-[#BDBDBD] rounded-[20px]">
                             <img src="/img/globo2.jpg" alt="" class="max-w-36 max-h-32 m-auto h-auto ">
                         </figure>
-                        <div class="flex flex-col gap-4 max-w-32">
+                        <div class="flex flex-col gap-4 min-w-32">
+                            <h1 class="font-poppins font-poppins-600 text-colorText text-xl text-wrap">Nombre del productos
+                            </h1>
+                            <p class="font-poppins font-poppins-400 text-xl">Color: <span>Verde</span></p>
+                            <p class="font-poppins font-poppins-400 text-xl">Tamañano: <span>30</span></p>
+                        </div>
+                    </td>
+                    <td>
+                        <span>$3000</span>
+                    </td>
+                    <td>
+                        <button class=" px-2  bg-[#EEE] border boder-solid border-[#BDBDBD]">-</button>
+                        <span class="px-6 py-[1.5px] border-y boder-solid bg-[#EEE] border-[#BDBDBD]">1</span>
+                        <button class="px-2 bg-[#EEE] border boder-solid border-[#BDBDBD]">+</button>
+
+                    </td>
+                    <td>
+                        <span>$3000</span>
+                    </td>
+                    <td>
+                        <button class=" px-2 bg-[#EEE] border boder-solid border-[#BDBDBD] rounded-full">x</button>
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class=" flex flex-row justify-center items-start gap-4 mt-5 mr-10">
+                        <figure class="p-3 border-[0.5px] mb-3 border-solid border-[#BDBDBD] rounded-[20px]">
+                            <img src="/img/globo2.jpg" alt="" class="max-w-36 max-h-32 m-auto h-auto ">
+                        </figure>
+                        <div class="flex flex-col gap-4 min-w-32">
                             <h1 class="font-poppins font-poppins-600 text-colorText text-xl text-wrap">Nombre del productos
                             </h1>
                             <p class="font-poppins font-poppins-400 text-xl">Color: <span>Verde</span></p>
@@ -42,15 +70,15 @@
                         <span>$3000</span>
                     </td>
                     <td>
-                        X
+                        <button class=" px-2 bg-[#EEE] border boder-solid border-[#BDBDBD] rounded-full">x</button>
                     </td>
                 </tr>
-                <tr>
+                <tr class="relative">
                     <td class=" flex flex-row justify-center items-start gap-4 mt-5 mr-10">
-                        <figure class="p-3 border-[0.5px] border-solid border-[#BDBDBD] rounded-[20px]">
+                        <figure class="p-3 border-[0.5px] mb-3 border-solid border-[#BDBDBD] rounded-[20px]">
                             <img src="/img/globo2.jpg" alt="" class="max-w-36 max-h-32 m-auto h-auto ">
                         </figure>
-                        <div class="flex flex-col gap-4 max-w-32">
+                        <div class="flex flex-col gap-4 min-w-32">
                             <h1 class="font-poppins font-poppins-600 text-colorText text-xl text-wrap">Nombre del productos
                             </h1>
                             <p class="font-poppins font-poppins-400 text-xl">Color: <span>Verde</span></p>
@@ -61,21 +89,44 @@
                         <span>$3000</span>
                     </td>
                     <td>
-                        <div class="bg-[#EEE]  flex flex-row justify-start items-center">
-                            <button class=" px-2 border boder-solid border-[#BDBDBD]">-</button>
-                            <span class="px-6 border-y boder-solid bg-[#EEE] border-[#BDBDBD]">1</span>
-                            <button class="px-2 bg-[#EEE] border boder-solid border-[#BDBDBD]">+</button>
-                        </div>
+                        <button class=" px-2 bg-[#EEE] border boder-solid border-[#BDBDBD]">-</button>
+                        <span class="px-6 py-[1.5px] border-y boder-solid bg-[#EEE] border-[#BDBDBD]">1</span>
+                        <button class="px-2 bg-[#EEE] border boder-solid border-[#BDBDBD]">+</button>
+
                     </td>
                     <td>
                         <span>$3000</span>
                     </td>
                     <td>
-                        X
+                        <button class=" px-2 bg-[#EEE] border boder-solid border-[#BDBDBD] rounded-full">x</button>
                     </td>
                 </tr>
             </tbody>
         </table>
+        <section
+            class="w-[400px] border border-solid border-primary max-w-[426px] max-h-[499px] flex flex-col justify-start ">
+            <h1 class="bg-[#E2F4FF] text-center py-4">Resumen de compra</h1>
+
+            <article>
+                <div class="flex flex-col justify-between">
+                    <div class="flex flex-row justify-between p-8">
+                        <p class="font-poppins font-poppins-400 text-xl">Subtotal</p>
+                        <span class="font-poppins font-poppins-500 text-xl">$3000</span>
+                    </div>
+                    <hr class="border border-solid border-[#AFAFAF] m-3">
+                    <form class="flex flex-row justify-between p-8" action="">
+                        <input type="text" name="" id="" placeholder="Ingrese cupón"
+                            class="font-poppins font-poppins-400 p-5 rounded-2xl">
+                        <button>Aplicar</button>
+                    </form>
+                    <hr class="border border-solid border-[#AFAFAF] m-3">
+                    <div class="flex flex-row justify-between p-8">
+                        <h1 class="font-poppins font-poppins-400 text-xl">Total a pagar:</h1>
+                        <span class="font-poppins font-poppins-500 text-xl">$3000</span>
+                    </div>
+                </div>
+            </article>
+        </section>
     </section>
 </template>
 
@@ -87,5 +138,13 @@
     font-style: normal;
     font-weight: 500;
     line-height: normal;
+}
+
+tr {
+    border-bottom: 1px solid #c3c3c3;
+}
+
+tr.border-b {
+    border-bottom: 1px solid #FF90BC;
 }
 </style>
--- pages/cart.vue
+++ pages/cart.vue
@@ -5,7 +5,7 @@ import AppFooter from '~/components/AppFooter.vue';
 </script>
 
 <template>
-    <main class="max-w-[1500px] m-auto mt-10 flex flex-row">
+    <main class="w-[1500px] m-auto mt-10 flex flex-row">
         <AppCart />
     </main>
     <footer class="mt-10 mb-5 bg-withOutStock h-[420px] py-2">
